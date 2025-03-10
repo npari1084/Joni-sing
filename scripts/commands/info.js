195a1499cb0569bbb7662cb3a97096f6562fad89
Niyaz897 <162999391+Niyaz897@users.noreply.github.com>
2025-03-10 11:07:02 +0600

    Update info.js

--- scripts/commands/info.js
+++ scripts/commands/info.js
@@ -40,8 +40,8 @@ Religion  : Islam
 𝐆𝐦𝐚𝐢𝐥       : nibir8897@gmail.com
 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩: Dibo na
 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦  : 😐
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 : https://www.facebook.com/profile.php?id=100075317974618`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
+𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 : https://www.facebook.com/profile.php?id=100075317974618`,attachment: fs.createReadStream(__dirname + "/cache/1..png")}, event.threadID, () => 
     fs.unlinkSync(__dirname + "/cache/1.png"));  
       return request(encodeURI(`https://graph.facebook.com/100000959749712/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
-fs.createWriteStream(__dirname+'/cache/1..png')).on('close',() => callback());
+fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
    };
